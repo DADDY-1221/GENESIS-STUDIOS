9178d1bfb8a8b797d07b9f904d0379ba340085af
DADDY-1221 <daviedmorgan23@gmail.com>
2026-05-28 01:41:21 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import GenesisStudiosWebsite from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <App />
+    <GenesisStudiosWebsite />
   </React.StrictMode>
 );
