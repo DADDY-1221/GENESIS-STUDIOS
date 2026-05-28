b72a28e2bb741d73e2054577341c31c96a79a7ac
DADDY-1221 <daviedmorgan23@gmail.com>
2026-05-29 01:01:44 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-import React from 'react';
-import Hero from './Hero';
-import BrandStory from './BrandStory';
-import TravelShowcase from './TravelShowcase';
-import CreatorJourney from './CreatorJourney';
-import FutureExpansion from './FutureExpansion';
-
 export default function App() {
   return (
-    <div>
-      <Hero />
-      <BrandStory />
-      <TravelShowcase />
-      <CreatorJourney />
-      <FutureExpansion />
+    <div
+      style={{
+        background: "#020617",
+        color: "white",
+        minHeight: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        fontFamily: "Arial",
+      }}
+    >
+      <h1 style={{ fontSize: "60px" }}>
+        GENESIS STUDIOS
+      </h1>
+
+      <p style={{ fontSize: "20px", opacity: 0.8 }}>
+        Cinematic Travel & Luxury Experiences
+      </p>
     </div>
   );
 }
