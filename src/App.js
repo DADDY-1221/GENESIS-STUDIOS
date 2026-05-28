472e40879312329947a12a460dc5672b0da628b8
DADDY-1221 <daviedmorgan23@gmail.com>
2026-05-29 00:22:50 +0300

    Add CreatorJourney component and update imports

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import Hero from './Hero';
 import BrandStory from './BrandStory';
-import TravelShowcase from './TravelShowcase'; // 1. Added the import
+import TravelShowcase from './TravelShowcase';
+import CreatorJourney from './CreatorJourney'; // 1. Added the import
 
 function App() {
   return (
     <div>
       <Hero />
       <BrandStory />
-      <TravelShowcase /> {/* 2. Placed it right below the Brand Story */}
+      <TravelShowcase />
+      <CreatorJourney /> {/* 2. Placed it right below the Showcase */}
     </div>
   );
 }
