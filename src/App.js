88288ef224324a7e1083cfe8e92da99f452cf0fd
DADDY-1221 <daviedmorgan23@gmail.com>
2026-05-29 01:28:51 +0300

    Refactor App component to use child components
    Refactored App component to use child components for better structure.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
+import React from 'react';
+import Hero from './Hero';
+import BrandStory from './BrandStory';
+import TravelShowcase from './TravelShowcase';
+import CreatorJourney from './CreatorJourney';
+import FutureExpansion from './FutureExpansion';
+
 export default function App() {
   return (
-    <div
-      style={{
-        background: "#020617",
-        color: "white",
-        minHeight: "100vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        flexDirection: "column",
-        fontFamily: "Arial",
-      }}
-    >GENESIS STUDIOS 
-      <h1 style={{ fontSize: "60px" }}>
-        GENESIS STUDIOS
-      </h1>
-
-      <p style={{ fontSize: "20px", opacity: 0.8 }}>
-        Cinematic Travel & Luxury Experiences
-      </p>
+    <div>
+      <Hero />
+      <BrandStory />
+      <TravelShowcase />
+      <CreatorJourney />
+      <FutureExpansion />
     </div>
   );
 }
