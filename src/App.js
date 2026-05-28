1baa82133cffd7036a5f0e01af9bff2a0d6e16dc
DADDY-1221 <daviedmorgan23@gmail.com>
2026-05-29 00:05:55 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Hero from './Hero';
-import BrandStory from './BrandStory'; // 1. Added the import
+import BrandStory from './BrandStory';
+import TravelShowcase from './TravelShowcase'; // 1. Added the import
 
 function App() {
   return (
     <div>
       <Hero />
-      <BrandStory /> {/* 2. Placed it right below the Hero */}
+      <BrandStory />
+      <TravelShowcase /> {/* 2. Placed it right below the Brand Story */}
     </div>
   );
 }
