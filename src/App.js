11d5d385c794c611d1674e0c522cd51af5b9ffb9
DADDY-1221 <daviedmorgan23@gmail.com>
2026-05-31 23:22:12 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ export default function App() {
       style={{
         backgroundColor: "#020617",
         color: "white",
-        fontFamily: "Arial",
+        fontFamily: "Inter, sans-serif",
         minHeight: "100vh",
       }}
     >
