b7e655bb70f1a5209f54b9ed262891cfb891330d
DADDY-1221 <daviedmorgan23@gmail.com>
2026-06-04 00:54:34 +0300

    Add Navbar component to App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Hero from './Hero';
+import Navbar from './Navbar'; // Import the new file
 
 function App() {
   return (
     <div className="App">
+      <Navbar /> {/* Add it here */}
       <Hero />
     </div>
   );
