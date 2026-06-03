17cd31d3826342a8e843eca24ff1ef62eeb9f895
DADDY-1221 <daviedmorgan23@gmail.com>
2026-06-04 00:57:32 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Hero from './Hero';
-import Navbar from './Navbar'; // Import the new file
+import Navbar from './Navbar';
+import TravelShowcase from './TravelShowcase'; // Import the new file
 
 function App() {
   return (
-    <div className="App">
-      <Navbar /> {/* Add it here */}
+    <div className="App" style={{ backgroundColor: '#050505', minHeight: '100vh' }}>
+      <Navbar />
       <Hero />
+      <TravelShowcase /> {/* Add the showcase below the Hero */}
     </div>
   );
 }
